bf63b5994ebb2b64b3090fbbe1dd2b42d7a4cb60
Cameron Cundiff <github@ckundo.com>
2019-06-24 23:54:02 -0400

    Add gesture actions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const confirm = () => { keypress("e"); };
 const back = () => { keypress("q"); };
 
 const startCamera = (video) => {
-  video.width = video.width || 640;
+  video.width = video.width || 320;
   video.height = video.height || video.width * (3 / 4)
 
   navigator.mediaDevices
@@ -25,20 +25,81 @@ const startCamera = (video) => {
       video.onloadedmetadata = () => {
         video.play()
       }
-      return video;
+      return true;
     }).catch(console.error);
 }
 
-document.querySelector("#gestures").onclick = () => {
-  const video = document.createElement("video");
-  document.appendChild(video);
-  video.style.position = "fixed";
-  video.style.top = 10;
-  video.style.left = 10;
+window.addEventListener("load", () => {
+  const button = document.createElement("button");
+  document.body.appendChild(button);
+  button.id = "gestures";
+  button.innerText = "Enable Gestures";
+  button.style.zIndex = 9999;
+  button.style.position = "fixed";
+  button.style.top = "10px";
+  button.style.right = "10px";
 
-  const video = await startCamera(video);
-  posenet.load().then(net => {
-    const pose = net.estimateSinglePose(video);
-    console.log(pose);
-  });
-}
+  document.querySelector("#gestures").addEventListener("click", async () => {
+    button.style.display = "none";
+    const video = document.createElement("video");
+    document.body.appendChild(video);
+
+    video.style.zIndex = 9999;
+    video.style.position = "fixed";
+    video.style.top = "10px";
+    video.style.right = "10px";
+
+    const started = await startCamera(video);
+    const net = await posenet.load({
+      architecture: 'ResNet50',
+      outputStride: 32,
+      inputResolution: 257,
+      quantBytes: 2
+    });
+
+    const detect = () => {
+      net.estimateSinglePose(video, { flipHorizontal: true }).then((pose) => {
+        const confident = 0.9;
+        const proximate = 7;
+        const nose = pose.keypoints.find(keypoint => keypoint.part === "nose");
+        const rWrist = pose.keypoints.find(keypoint => keypoint.part === "rightWrist");
+        const lWrist = pose.keypoints.find(keypoint => keypoint.part === "leftWrist");
+        const rShoulder = pose.keypoints.find(keypoint => keypoint.part === "rightShoulder");
+        const lShoulder = pose.keypoints.find(keypoint => keypoint.part === "leftShoulder");
+        const rEar = pose.keypoints.find(keypoint => keypoint.part === "rightEar");
+        const lEar = pose.keypoints.find(keypoint => keypoint.part === "leftEar");
+
+        if (rWrist.score > confident && rEar.score > confident) {
+          const wristDist = Math.sqrt((rWrist.position.x - rEar.position.x)^2 + (rWrist.position.y - rEar.position.y)^2);
+
+          if (wristDist < proximate) {
+            console.log("ding", "confirm");
+            confirm();
+          }
+        }
+
+        if (rWrist.score > confident && rShoulder.score > confident) {
+          rDist = Math.sqrt((rWrist.position.x - rShoulder.position.x)^2 + (rWrist.position.y - rShoulder.position.y)^2);
+
+          if (rDist < proximate) {
+            console.log("ding", "pan right");
+            right();
+          }
+        }
+
+        if (lWrist.score > confident && lShoulder.score > confident) {
+          lDist = Math.sqrt((lWrist.position.x - lShoulder.position.x)^2 + (lWrist.position.y - lShoulder.position.y)^2);
+
+          if (lDist < proximate) {
+            console.log("ding", "pan left");
+            left();
+          }
+        }
+
+        requestAnimationFrame(detect);
+      });
+    }
+
+    detect();
+  }, { once: true });
+});
